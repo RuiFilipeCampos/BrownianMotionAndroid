27e571fa958e4f0dd55ba63886ce8f55a9c2b09d
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-07 00:11:08 +0100

    my first java classes

--- BrownianMotionAndroid/app/src/main/java/com/example/brownianmotionandroid/MainActivity.java
+++ BrownianMotionAndroid/app/src/main/java/com/example/brownianmotionandroid/MainActivity.java
@@ -4,6 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// the usual geometry stuff
+class Vector{
+    private double x, y, z;
+
+    Vector(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+
+    public Vector plus(Vector other){
+        return new Vector(this.x + other.x, this.y + other.y, this.z + other.z);
+    }
+
+    public Vector minus(Vector other){
+        return new Vector(this.x - other.x, this.y - other.y, this.z - other.z);
+    }
+
+    public double dot(Vector other){
+        return this.x*other.x + this.y*other.y + this.z*other.z;
+    }
+
+    public void rotate(Vector axis){
+        // will need to do rotation thingies
+        return;
+    }
+}
+
+class Particle{
+
+
+
+}
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
